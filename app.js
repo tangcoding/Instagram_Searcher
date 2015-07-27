776ede76ba80e605cdb3a5c0d17be5f4613f2aab
Wenjie Tang <twj916@hotmail.com>
2015-07-27 13:10:42 -0500

    finished version for thinkful project

--- app.js
+++ app.js
@@ -1,42 +1,33 @@
 var app = angular.module('app',[]);
 
-app.controller('search_controller',function($scope, $http){
+app.controller('search_controller',function($scope, $http, $timeout){
 
-
-    $scope.search_success=false;
-    $scope.fail = false;
-    $scope.doing_searching = false;
-
-  function wait() {
-    return $q(function(resolve, reject){
-      $timeout(function() {
-        resolve();
-      }, 2000);
-    });
-  }
 
 	$scope.submit = function(){
 
-		$scope.doing_searching = true;
+		$scope.message = 'Searching Instagram for photos tagged with "' + $scope.tag + '".';
 
 		var url = 'https://api.instagram.com/v1/tags/' + $scope.tag + '/media/recent';
 
-		var request = {callback: 'JSON_CALLBACK',
-		              client_id: '7801b4ae0486431e9e81f5301fdcb006'};
+		var request = {
+      count: 21,
+      callback: 'JSON_CALLBACK',
+		  client_id: '7801b4ae0486431e9e81f5301fdcb006'};
 
 		$http({url:url, params:request, method: 'JSONP'})
 		.success(success)
 		.error(error);
 
 		function success(data, status, headers, config){
-
-		  $scope.search_success=true;
-          $scope.results = data.data;
-         
-        }
-       function error(error, status, headers, config){
-       	$scope.fail = true;
-        }
+      $timeout(
+        $scope.message = 'We found ' + data.data.length + ' results for ' + $scope.tag + '.',
+        1000);
+      $scope.results = data.data;
+    }
+       
+    function error(error, status, headers, config){
+      $scope.message = 'There are some errors when processing your request.';
+    }
 
 
 	}
